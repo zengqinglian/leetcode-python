937662f09f8e915fb34bef5c532434eaed0ea3ba
Eric Zeng <eric.zeng@capco.com>
2017-03-07 18:13:24 +0000

    leetcode - Codec solution.

---
+++ leetcode/src/encodeAndDecodeTinyURL/Codec.py
@@ -0,0 +1,25 @@
+class Codec:
+    url = list();
+    
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+        
+        :type longUrl: str
+        :rtype: str
+        """
+        self.url.append(longUrl)
+        return len(self.url)
+        
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+        
+        :type shortUrl: str
+        :rtype: str
+        """
+        return self.url[shortUrl-1]
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
